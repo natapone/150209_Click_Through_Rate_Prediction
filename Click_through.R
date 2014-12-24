6682ef0f8b99bc5fc834d59e2c3cf38a9991aa22
Natapone C <natapone@gmail.com>
2014-12-24 11:56:05 +0700

    check test without train

--- Click_through.R
+++ Click_through.R
@@ -1,19 +1,15 @@
 # USAGE
 # source("Click_through.R")
+# train_set = read_data('train',1000)
+# test_set = read_data('test')
+# validate_count_unique(train_set)
+# test_without_train(train_set, test_set)
 
 require(data.table)
 
-# http://stackoverflow.com/questions/1727772/quickly-reading-very-large-tables-as-dataframes-in-r/15058684#15058684
-
-# validate ==> 
-# http://stat.ethz.ch/R-manual/R-patched/library/stats/html/aggregate.html
-# http://stats.stackexchange.com/questions/8225/how-to-summarize-data-by-group-in-r
-
-
-# Convert
-# Round time => convert to xxxx0000
-# convert to time => http://stackoverflow.com/questions/13456241/convert-unix-epoch-to-date-object-in-r
-
+interpret_data <- function( data, type="train") {
+    
+}
 
 read_data <- function(file_name, limit=0) {
     # read data by type
@@ -27,4 +23,70 @@ read_data <- function(file_name, limit=0) {
     
     data
 }
+
+validate_count_unique <- function(data) {
+    # loop through columns
+    
+    for (col_name in colnames(data)) {
+        # print(col_name)
+        
+        col_data = data[[col_name]]
+        col_data = unique(col_data) # only unique
+        
+        #print (length(data[[col_name]])) # count data
+        print( c(col_name , length(col_data)) )
+        
+        
+        # test[,i]
+    }
+    
+}
+
+test_without_train <- function(train, test) {
+    
+    field_list = c(
+        "C1",
+        "banner_pos",
+        "site_id",
+        "site_domain",
+        "site_category",
+        "app_id",
+        "app_domain",
+        "app_category",
+        "device_id",
+        "device_ip",
+        "device_model",
+        "device_type",
+        "device_conn_type",
+        "C14",
+        "C15",
+        "C16",
+        "C17",
+        "C18",
+        "C19",
+        "C20",
+        "C21"
+    )
+    
+    for (col_name in field_list) {
+        
+        col_data_train = train[[col_name]]
+        col_data_train = unique(col_data_train)
+        
+        col_data_test = test[[col_name]]
+        col_data_test = unique(col_data_test)
+        
+        # test that not in train
+        miss = setdiff(col_data_test, col_data_train)
+        
+        print ( 
+            c(
+                col_name,
+                length(miss)
+            )
+        )
+    }
+    
+}
+
 
